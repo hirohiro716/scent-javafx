db7ae1fc83eb46ffa32b9528c64e0ba7f97925fa
hiro <hiro@nova>
2018-09-25 11:25:41 +0900

    Fixed bug of AutoCompleteTextField class

--- src/com/hirohiro716/javafx/control/AutoCompleteTextField.java
+++ src/com/hirohiro716/javafx/control/AutoCompleteTextField.java
@@ -283,7 +283,7 @@ public class AutoCompleteTextField extends LimitTextField {
      */
     private void createOnlyFirstItems() {
         AutoCompleteTextField field = AutoCompleteTextField.this;
-        if (field.popup != null || field.getParent() == null) {
+        if (field.popup != null || field.getParent() == null || this.getParent().getScene() == null) {
             return;
         }
         // リストビューを作成して設定
