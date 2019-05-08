0e9ab910f2c04c95ee100f3366611eca72e78497
hiro <hiro@nova>
2019-05-08 11:45:31 +0900

    Added class of IMEOffToggleButton

---
+++ src/com/hirohiro716/javafx/control/IMEOffToggleButton.java
@@ -0,0 +1,47 @@
+package com.hirohiro716.javafx.control;
+
+import com.hirohiro716.javafx.IMEHelper;
+import com.hirohiro716.robot.InterfaceTypingRobotJapanese.IMEMode;
+
+import javafx.application.Platform;
+import javafx.scene.Node;
+import javafx.scene.control.ToggleButton;
+
+/**
+ * フォーカスした際にIMEをOFFにするボタン.
+ * @author hiro
+ *
+ */
+public class IMEOffToggleButton extends ToggleButton {
+    
+    /**
+     * コンストラクタ.
+     */
+    public IMEOffToggleButton() {
+        this("", null);
+    }
+    
+    /**
+     * コンストラクタ.
+     * @param text
+     */
+    public IMEOffToggleButton(String text) {
+        this(text, null);
+    }
+    
+    /**
+     * コンストラクタ.
+     * @param text
+     * @param graphic
+     */
+    public IMEOffToggleButton(String text, Node graphic) {
+        super(text, graphic);
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                IMEHelper.apply(IMEOffToggleButton.this, IMEMode.OFF);
+            }
+        });
+    }
+    
+}
